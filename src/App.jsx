73d236dcf07a7910c1213fd3d512013aebae7cd0
MohamedDharik <mohameddharik.h.2021.ece@ritchennai.edu.in>
2024-07-16 17:22:51 +0530

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import {Button } from 'react-bootstrap'
+import {Button, Table } from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
 
@@ -47,70 +47,70 @@ function App() {
   }
    
   return(<>
-   <h1 className='h1 text-info text-center'>Api Fetched Data</h1>
-   
-    <div className='container-fluid table table-bordered small w- mt-4 px-0'>
-      <div className="row px-0 ms-2 me-3">
-        <div className="col  ">
-         <th>ID</th></div>
-        <div className="col  ">
-         <th >Name</th>
-        </div>
-        <div className="col">
-         <th >Email</th>
-        </div>
-        <div className="col">
-          <th >Phone</th>
-        </div>
-        <div className="col"> 
-          <th>Website</th>
-        </div>
-      </div>
-      <div className='row px-0 ms-2 me-3'>
-        {user.map(users =>
-        <td key={users.id}>
-          <div className="row px-0 ms-1">
-          <div className="col px-0  "><td >{users.id}</td></div>
-          <div className="col px-0"><td>{users.name}</td></div>
-          <div className="col px-0"><td>{users.email}</td></div>
-          <div className="col px-0"><td>{users.phone}</td></div>
-          <div className="col px-0"><td>{users.website}</td></div>
+
+<h1 className='h1 text-info text-center'>Api Fetched Data</h1>
+    <div className='container'>
+      <div className='row justify-content-center'>
+        <div className='col-12 col-md-10 col-lg-8'>
+          <div className='table-responsive mt-4'>
+            <table className='table table-bordered table-striped'>
+              <thead>
+                <tr>
+                  <th>ID</th>
+                  <th>Name</th>
+                  <th>Email</th>
+                  <th>Phone</th>
+                  <th>Website</th>
+                </tr>
+              </thead>
+              <tbody>
+                {user.map(users => (
+                  <tr key={users.id}>
+                    <td>{users.id}</td>
+                    <td>{users.name}</td>
+                    <td>{users.email}</td>
+                    <td>{users.phone}</td>
+                    <td>{users.website}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
           </div>
-         
-        </td>
-          )
-        }
+        </div>
       </div>
+    </div>
+
+
+
       <p className='h3 mt-3 ms-4'>Add User</p>
       <tfoot className=' card ms-3 w-25 mt-3'>
-        <div className="card border-info mx-auto">
- 
+        <div className="card border-muted mx-auto">
           <div className="row ">
-          <div className="col-12 ms-5 mt-1 ">
+          <div className="col-12 ms-4 mt-1 ">
             
           <td><input type="text" placeholder='Enter name..' value={newName} onChange={(e) => setNewName(e.target.value)} autoFocus></input></td>
           </div>
 
-          <div className="col-12 ms-5 mt-1">
+          <div className="col-12 ms-4 mt-1">
           <td><input type="text" placeholder='Enter email..' value={newEmail} onChange={(e) => setNewEmail(e.target.value)}></input></td>
           </div>
 
-          <div className="col -12 ms-5 mt-1">
+          <div className="col -12 ms-4 mt-1">
           <td><input type="text" placeholder='Enter phone..' value={newPhone} onChange={(e) => setNewPhone(e.target.value)}></input></td>
           </div>
 
-          <div className="col-12 ms-5 mt-1">
+          <div className="col-12 ms-4 mt-1">
           <td><input type="text" placeholder='Enter website..' value={newWebsite} onChange={(e) => setNewWebsite(e.target.value)}></input></td>
           </div>
 
-          <div className="col-12 ms-5 mt-1">
+          <div className="col-12 ms-4 mt-1">
           <td><Button onClick={addUser} variant="outline-success">Add User</Button></td>
           </div>
           </div>
-
+         
           </div>
       </tfoot>
-    </div>
+      
    
     </>
   );
